593a11456f747ade4e09b67f667c4e5065015b9b
Maksym <maxim240896@icloud.com>
2024-05-15 13:30:09 +0300

    seventh commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import { useEffect, useState } from 'react';
 import './App.css';
 import Header from './Header/Header';
 import Main from './Main/Main';
+import Rectangle29 from '../src/image/Rectangle 29.svg'
+import { useEffect, useState } from 'react';
+import Loading from './components/Loading';
 
 function App() {
+
+  const [isImageLoaded, setIsImageLoaded] = useState(false)
+  const img = new Image()
+  img.src = Rectangle29
+  img.onload = () => setIsImageLoaded(true)
+  useEffect(() => {
+
+  }, [])
+
   return (
     <div className="App">
+      {!isImageLoaded && <Loading />}
       <Header />
       <Main />
     </div>
